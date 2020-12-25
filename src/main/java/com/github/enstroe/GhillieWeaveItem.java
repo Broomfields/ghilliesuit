9b567150a99da8426871453ee29f1c338016e80f
Shaun Broomfield <36537416+Enstroe@users.noreply.github.com>
2020-12-25 03:16:31 +0000

    Added main crafting component
    still need lang, texture, model, recipe
    integration

---
+++ src/main/java/com/github/enstroe/GhillieWeaveItem.java
@@ -0,0 +1,10 @@
+package com.github.enstroe;
+
+import net.minecraft.item.Item;
+// import net.minecraft.item.ItemGroup;
+
+public class GhillieWeaveItem extends Item {
+    public GhillieWeaveItem(Settings settings) {
+        super(settings);
+    }
+}
--- src/main/java/com/github/enstroe/Ghilliesuit.java
+++ src/main/java/com/github/enstroe/Ghilliesuit.java
@@ -1,13 +1,24 @@
 package com.github.enstroe;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Ghilliesuit implements ModInitializer {
+
+	private static final Item GHILLIE_WEAVE = new GhillieWeaveItem(new Item.Settings().group(ItemGroup.MATERIALS).maxCount(64));
+
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+
+		Registry.register(Registry.ITEM, new Identifier("ghilliesuit", "ghillie_weave"), GHILLIE_WEAVE);
+
 
 		System.out.println("Hello Fabric world!");
 	}
