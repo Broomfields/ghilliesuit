3b2f9dc29d057c6e2c61730512655a42d636a830
Shaun Broomfield <36537416+Enstroe@users.noreply.github.com>
2020-12-25 02:32:57 +0000

    more setup

--- src/main/java/com/github/enstroe/Ghilliesuit.java
+++ src/main/java/com/github/enstroe/Ghilliesuit.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package com.github.enstroe;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class Ghilliesuit implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
